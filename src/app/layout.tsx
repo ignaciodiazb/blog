6e815457d8ee257d99d01fc43fcbc84cd7cddf54
Ignacio Díaz <ignacio.db@icodesx.com>
2023-09-28 11:57:19 -0300

    Added global styles to home page layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import "./globals.css";
 import "./layout.css";
 
 const inter = Inter({ subsets: ["latin"] });
