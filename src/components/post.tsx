0fab8b809b56daa2ccfa5f5419541a7e873628cf
Ignacio Díaz <ignacio.db@icodesx.com>
2023-10-07 23:11:38 -0300

    Updated post component

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,28 +1,24 @@
 import Link from "next/link";
 
-interface PostProps {
+interface Props {
   date: string;
   intro: string;
+  lang: string;
   readingTime: number;
   slug: string;
   title: string;
 }
 
-export default function Post(props: PostProps) {
-  const { date, intro, slug, readingTime, title } = props;
-
+export default function Post({ date, intro, lang, slug, readingTime, title }: Props) {
   return (
     <article>
       <h3 className={"text-lg font-medium"}>
-        <Link className={"underline"} href={`/blog/${slug}`}>
+        <Link className={"underline"} href={`/${lang}/blog/${slug}`}>
           {title}
         </Link>
       </h3>
       <p className={"text-xs text-slate-600"}>
-        <time className={""} dateTime={date}>
-          {date}
-        </time>{" "}
-        &middot; {readingTime} min read
+        <time dateTime={date}>{date}</time> &middot; {readingTime} min read
       </p>
       <p className={""}>{intro}</p>
     </article>
