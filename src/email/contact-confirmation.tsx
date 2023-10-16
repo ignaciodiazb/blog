0474ea521874210b5af386b26de155c13efb5110
Ignacio Díaz <ignacio.db@icodesx.com>
2023-10-16 15:44:47 -0300

    Updated contact confirmation email template

--- src/email/contact-confirmation.tsx
+++ src/email/contact-confirmation.tsx
@@ -1,3 +1,12 @@
+import { Body } from "@react-email/body";
+import { Container } from "@react-email/container";
+import { Head } from "@react-email/head";
+import { Heading } from "@react-email/heading";
+import { Hr } from "@react-email/hr";
+import { Html } from "@react-email/html";
+import { Section } from "@react-email/section";
+import { Text } from "@react-email/text";
+
 interface ContactConfirmationProps {
   email: string;
   message: string;
@@ -6,27 +15,66 @@ interface ContactConfirmationProps {
 
 export default function ContactConfirmation({ email, message, name }: ContactConfirmationProps) {
   return (
-    <div>
-      <p>
-        Hi <strong>{name}</strong>.
-      </p>
-      <p>Thank you for reaching out. I have received your message and will reply as soon as I can.</p>
-      <p>Below you will find a copy.</p>
-      <div>
-        <p>
-          <strong>Name</strong>
-        </p>
-        <p>{name}</p>
-        <p>
-          <strong>Email</strong>
-        </p>
-        <p>{email}</p>
-        <p>
-          <strong>Message</strong>
-        </p>
-        <p>{message}</p>
-      </div>
-      <p>This is an automated message.</p>
-    </div>
+    <Html dir={"ltr"} lang={"en"}>
+      <Head />
+      <Body style={body}>
+        <Container style={container}>
+          <Section style={box}>
+            <Heading as={"h1"}>
+              Hi <b>{name}</b>.
+            </Heading>
+            <Text style={paragraph}>
+              Thank you for reaching out. I have received your message and will reply as soon as I can.
+            </Text>
+            <Text style={paragraph}>Below you will find a copy.</Text>
+            <Text style={paragraph}>Best regards,</Text>
+            <Text style={paragraph}>Ignacio Díaz</Text>
+            <Hr style={hr} />
+            {/* <Text style={paragraph}>Summary</Text> */}
+            <Heading as={"h2"}>Summary</Heading>
+            <Text style={paragraph}>
+              <b>Name: </b>
+              {name}
+            </Text>
+            <Text style={paragraph}>
+              <b>Email: </b>
+              {email}
+            </Text>
+            <Text style={paragraph}>
+              <b>Message: </b>
+              {message}
+            </Text>
+          </Section>
+        </Container>
+      </Body>
+    </Html>
   );
 }
+
+const body = {
+  backgroundColor: "#f6f9fc",
+  fontFamily: '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Ubuntu,sans-serif',
+};
+
+const box = {
+  padding: "0 48px",
+};
+
+const container = {
+  backgroundColor: "#ffffff",
+  margin: "0 auto",
+  marginBottom: "64px",
+  padding: "20px 0 48px",
+};
+
+const hr = {
+  borderColor: "#e6ebf1",
+  margin: "20px 0",
+};
+
+const paragraph = {
+  color: "#525f7f",
+  fontSize: "16px",
+  lineHeight: "24px",
+  textAlign: "left" as const,
+};
